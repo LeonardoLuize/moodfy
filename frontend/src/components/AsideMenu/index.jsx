cc2b1b1afb725cc9ef8acaa243567c8947470023
Leonardo Luize <leonardo.luize2@gmail.com>
2023-04-14 10:54:08 -0300

    feat: add lateral bar

--- frontend/src/components/AsideMenu/index.jsx
+++ frontend/src/components/AsideMenu/index.jsx
@@ -1,15 +1,18 @@
 import {
   Box,
+  Button,
   Drawer,
   DrawerBody,
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
   Icon,
+  Img,
   useDisclosure,
 } from "@chakra-ui/react";
 import theme from "../../theme";
-import {TiHome} from "react-icons/ti"
+import { RiHomeFill } from "react-icons/ri";
+import LogoIcon from "../../assets/logo.svg"
 
 export function AsideMenu() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -20,14 +23,24 @@ export function AsideMenu() {
         width="100px"
         height="100%"
         display="flex"
-        justifyContent="center"
-        alignItems="self-start"
+        flexDir="column"
+        justifyContent="space-between"
+        alignItems="center"
         p={8}
         bg={theme.colors.menuBackground["500"]}
       >
-       <Box>
-        <Icon as={TiHome} color={theme.colors.brand["500"]} w={7} h={7}/>
-       </Box>
+        <Box>
+          <Icon
+            as={RiHomeFill}
+            _hover={{color: theme.colors.brand["700"]}}
+            transition={"all .2s"}
+            cursor="pointer"
+            color={theme.colors.brand["500"]}
+            w={7}
+            h={7}
+          />
+        </Box>
+        <Img src={LogoIcon} w={7} h={7} />
       </Box>
       <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
         <DrawerOverlay />
