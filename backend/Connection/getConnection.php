935ea2681da2b8cd17d09619d396b796f2dbc5c5
leonardo falango <leo_falango@hotmail.com>
2023-05-31 11:50:17 -0300

    Fixed: Connection

--- backend/Connection/getConnection.php
+++ backend/Connection/getConnection.php
@@ -2,7 +2,9 @@
 
 $servername = "Moodfy"
 $username = "root"
-$password = "root"
-function getConnection()
+$password = "root" # Hide the password
+function getConnection() {
     return new sqli($servername, $username, $password)
+}
+
 ?>
--- backend/Connection/insertData.php
+++ backend/Connection/insertData.php
@@ -3,12 +3,21 @@ function insertPlace($name, $desc, $lat, $long, $photo)
 {
     include 'getConnection.php';
     $conn = getConnection();
-    if ($conn->connect_error)
+    if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+    }
     
     $query = "INSERT INTO Places (Name, Latitude, Longitude, Description, Photo) VALUES
-        ($name, $lat, $long, $desc, $photo)"
+        ($name, $lat, $long, $desc, $photo)";
     
+    $result = $conn->query($query);
+
+    if ($result === TRUE) {
+        echo "Inserted<br>" . $query . "<br>" . mysql_insert_id() . $name . $lat . $long . $desc . $photo;
+    } else {
+        echo "Error: " . $query . "<br>" . $conn->error;
+    }
     
+    $conn->close();
 }
 ?>
