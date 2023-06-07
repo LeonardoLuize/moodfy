5eb429802b9fd8e755d8e61cee9bbf0ab420a8a4
unknown <silmike14@yahoo.com.br>
2023-06-06 22:28:39 -0300

    feat: add card

---
+++ frontend/src/components/LocalsCard/index.jsx
@@ -0,0 +1,48 @@
+import {
+  Button,
+  Input,
+  Icon,
+  InputGroup,
+  InputRightElement,
+  Box,
+  Tag,
+  TagLabel,
+  TagCloseButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Text,
+  Textarea,
+  Heading,
+  Image
+} from "@chakra-ui/react";
+import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import {MapPin} from "@phosphor-icons/react"
+
+export function LocalsCard({data}) {
+  return (
+    <>
+      <Box display="flex" alignItems="center" gap={5} mt={5}>
+        <Box display="flex" rounded="xl" alignItems="center" justifyContent="center" w="100px" h="100px" overflow="hidden">
+          <Image
+            src="https://picsum.photos/100/100"
+            w="full"
+          />
+        </Box>
+
+        <Box gap={5}>
+          <Heading fontSize={20}>OPAO</Heading>
+          <Box display="flex" alingItems="center" justifyContent="center" gap={3}>
+            <Icon as={MapPin} fontSize={18} />
+            <Text>OPA</Text>
+          </Box>
+
+          <Box>
+              <Tag>3</Tag>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+}
--- frontend/src/components/MainContent/index.jsx
+++ frontend/src/components/MainContent/index.jsx
@@ -1,13 +1,15 @@
 import { Box, Img } from "@chakra-ui/react";
 import {MapDisplay} from "../Maps"
 import { Searchinput } from "../SearchInput";
+import {LocalsCard} from "../LocalsCard";
 
 export function MainContent(){
     return(
         <>
             <Box w="100%" h="100%" display="grid" gridTemplateColumns="2fr 1.5fr" >
                 <Box display="flex" flexDir="column" p={8}>
-                    <Searchinput />
+                    <Searchinput />   
+                    <LocalsCard />                  
                 </Box>
 
                 <Box display="flex" flexDir="column" p={8}>
