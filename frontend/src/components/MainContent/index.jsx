772bbb87ca2c706ee1a57e2b02e7ac0069c1ff59
Beatriz Jardim <paulabeatrizlj@hotmail.com>
2023-06-06 22:41:45 -0300

    feat: api request

--- frontend/src/components/MainContent/index.jsx
+++ frontend/src/components/MainContent/index.jsx
@@ -1,13 +1,17 @@
 import { Box, Img } from "@chakra-ui/react";
 import {MapDisplay} from "../Maps"
 import { Searchinput } from "../SearchInput";
+import { useSyncExternalStore } from "react";
+import {useState} from "react"
 
 export function MainContent(){
+    const [data, setData] = useState([])
+
     return(
         <>
             <Box w="100%" h="100%" display="grid" gridTemplateColumns="2fr 1.5fr" >
                 <Box display="flex" flexDir="column" p={8}>
-                    <Searchinput />
+                    <Searchinput setData={setData} />
                 </Box>
 
                 <Box display="flex" flexDir="column" p={8}>
