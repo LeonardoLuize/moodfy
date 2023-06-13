6606969c997b90cd4d7e07168233621775ae0de2
leonardo falango <leo_falango@hotmail.com>
2023-06-12 21:43:39 -0300

    add: get location by name

---
+++ backend/Adm/getLocationByName.php
@@ -0,0 +1,35 @@
+<?php
+/* Handle CORS */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: PUT, GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: X-Requested-With,Authorization,Content-Type');
+header('Access-Control-Max-Age: 86400');
+header('Content-Type: application/json; charset=utf-8');
+
+function getLocationByName($localName)
+{
+    include '../Connection/getConnection.php';
+    $conn = getConnection();
+
+    $query = "SELECT * FROM Places WHERE Name = %'$localName'%";
+    $result = $conn->query($query);
+    $conn->close();
+
+    $response = array(
+        'id' => $result['ID'],
+        'name' => $result['Name'],
+        'description' => $result['Description'],
+        'latitude' => $result['Latitude'],
+        'longitude' => $result['Longitude'],
+        'avaliation' => $result['Avaliation'],
+        'filter' => $result['Filter'],
+        'photo' => $result['Photo']
+    );
+
+    return json_encode($response);
+}
+
+$localName = $_GET['localName'];
+echo updateData($localName);
+
+?>
