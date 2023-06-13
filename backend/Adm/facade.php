0f7e14f550643124a044cea049f987a84c5a4ee2
Leonardo Luize <leonardo.luize2@gmail.com>
2023-06-12 23:54:18 -0300

    fix: verify values

--- backend/Adm/facade.php
+++ backend/Adm/facade.php
@@ -5,27 +5,25 @@ function decideQuery($userLat, $userLong, $filters, $localName)
     include('getLocationByFilter.php');
     include('getLocationByName.php');
     include('getLocation.php');
-    
-    if ($localName == "")
-    {
-        return getLocalFilter($userLat, $userLong, $filters);
-    }
-    
-    else if ($filters == "")
-    {
-        return getLocationByName($localName, $userLat, $userLong);
-    }
 
+    if ($localName != "")
+    {
+        return getLocationByName($userLat, $userLong, $filters);
+    }
+    else if ($filters != "")
+    {
+        return getLocalFilter($localName, $userLat, $userLong);
+    }
     else
     {
         return getLocation($filters, $localName, $userLat, $userLong);
     }
 }
 
-$userLat = $_GET['userLat'] ? $_GET['userLat'] : ""
-$userLong = $_GET['userLong'] ? $_GET['userLong'] : ""
-$filters = $_GET['filters'] ? $_GET['filters'] : ""
-$localName = $_GET['localName'] ? $_GET['localName'] : ""
+$userLat = isset($_GET['userLat']) ? $_GET['userLat'] : 0;
+$userLong = isset($_GET['userLong']) ? $_GET['userLong'] : 0;
+$filters = isset($_GET['filters']) ? $_GET['filters'] : "";
+$localName = isset($_GET['localName']) ? $_GET['localName'] : "";
 
 echo decideQuery($userLat, $userLong, $filters, $localName);
 ?>
