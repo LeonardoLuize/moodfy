2dc1009275365c401f1dcf24fe9de63a64cdb42a
Leonardo Luize <leonardo.luize2@gmail.com>
2023-06-12 23:36:15 -0300

    feat: merge

---
+++ backend/Adm/facade.php
@@ -0,0 +1,27 @@
+<?php
+
+function decideQuery($userLat, $userLong, $filters, $localName)
+{
+    include('getLocalsByFilter.php');
+    include('getLocationByName.php');
+    include('getLocation.php');
+    
+    if ($localName == "")
+    {
+        return getLocalFilter($userLat, $userLong, $filters);
+    }
+    
+    else if ($filters == "")
+    {
+        return getLocationByName($localName, $userLat, $userLong);
+    }
+
+    else
+    {
+        return getLocation($filters, $localName, $userLat, $userLong);
+    }
+}
+
+echo decideQuery($_GET['userLat'], $_GET['userLong'], $_GET['filters'], $_GET['localName']);
+
+?>
